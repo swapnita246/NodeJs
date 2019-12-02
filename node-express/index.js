503a9e07a352ae4465765995d02e65a3d65f8356
swapnita246 <swapnita161b246@gmail.com>
2019-12-02 10:42:52 +0530

    Express Router

--- node-express/index.js
+++ node-express/index.js
@@ -2,6 +2,7 @@ const express = require('express'),
  http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use('/dishes', dishRouter);
 
 app.all('/dishes', (req,res,next) => {
   res.statusCode = 200;
