b35a7de0fbde61fec0765bd4a9768a2b10801135
swapnita246 <swapnita161b246@gmail.com>
2019-12-02 10:21:08 +0530

    Express Serve Static File

--- node-express/index.js
+++ node-express/index.js
@@ -1,5 +1,5 @@
 const express = require('express'),
-     http = require('http');
+const http = require('http');
 
 const morgan = require('morgan');
 
@@ -13,7 +13,7 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
-  console.log(req.headers);
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
