3d9d5189638574e10b22544b009b0d2113928857
swapnita246 <swapnita161b246@gmail.com>
2019-12-03 19:29:16 +0530

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,5 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '574373753395602',
+        clientSecret: '45c26b1a8dfe27b6b183943943bdc1f8'
+    }
 }
-
